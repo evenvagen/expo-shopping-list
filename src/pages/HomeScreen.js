934738c6d4726e28dd5cf832b0f8e7f1a1cca6ab
Even Vågen <evenvagen@gmail.com>
2023-02-02 17:09:51 +0100

    Created HomeScreen.js

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,64 @@
+import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+
+export function HomeScreen() {
+  return (
+    <SafeAreaView>
+    <View> 
+        <View style={styles.container}>
+            <Text style={styles.headline}>Handleliste</Text>
+            <View style={styles.row}>
+                <TouchableOpacity style={styles.box}>
+                    <Text style={styles.boxText}>Vis</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.box}>
+                    <Text style={styles.boxText}>Legg til</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.row}>
+                <TouchableOpacity style={styles.box}>
+                    <Text style={styles.boxText}>Endre</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.box}>
+                    <Text style={styles.boxText}>Slette</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+        </View>
+    </SafeAreaView>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: "gray",
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5
+  },
+  headline: {
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: '25%'
+  },
+  boxText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
